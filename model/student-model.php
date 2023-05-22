0cd54dfed5b964a88e89ded09ba35e268c529160
Niklas Mårdby <niklas.mardby@gmail.com>
2023-05-22 14:27:17 +0200

    Add Student Model

--- model/student-model.php
+++ model/student-model.php
@@ -5,4 +5,29 @@
 class StudentModel
 {
     // TO-DO: metod för att läsa hela student-tabellen
+    private $pdo;
+
+    public function __construct()
+    {
+        $host = 'localhost';
+        $dbname = 'school-api';
+        $password = 'abc123';
+        $charset = 'utf8';
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false
+        ];
+        $pdo = new PDO($dsn, $dbname, $password, $options);
+        $this->pdo = $pdo;
+    }
+
+    function getStudents()
+    {
+        $statement = $this->pdo->prepare('SELECT * FROM students');
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
 }
